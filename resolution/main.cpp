47a5e8b208daaa28581db8e81302ea7e680c622e
Jonas Xavier <jonas.agx@gmail.com>
2018-03-19 21:42:58 -0700

    Initial Commit

---
+++ resolution/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  resolution
+//
+//  Created by Jonas Xavier on 3/19/18.
+//  Copyright © 2018 Jonas Xavier. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
